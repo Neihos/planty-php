772c0d6566be1b534394fd465df14d95536d56a6
Neihos <nathanael.detourbe@gmail.com>
2024-07-06 23:33:32 +0200

    planty

---
+++ app/public/wp-content/themes/planty/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <?php wp_head(); ?>
+
+    </head>
+
+    <body <?php body_class(); ?>>
+
+    <?php wp_body_open(); ?>
+
+    <header>
+        <div id="plantyHeader">
+            <legend id="logoPlanty">
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo home_url(); ?>/wp-content/uploads/2024/07/Logo_Planty.svg" alt="Logo Planty">
+                </a>
+            </legend>
+
+            <nav class="navBar">
+                <?php 
+                wp_nav_menu();             
+                ?>            
+
+                <div class="buttonCommander">
+				    <button type="button">Commander</button>
+			    </div>
+            </nav>
+        </div>     
+    </header>   
