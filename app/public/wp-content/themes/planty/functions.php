9b3aa13318f0b5229e708fec11bb571027c11de9
Neihos <nathanael.detourbe@gmail.com>
2024-07-08 23:57:38 +0200

    Mise en page de functions.php

--- app/public/wp-content/themes/planty/functions.php
+++ app/public/wp-content/themes/planty/functions.php
@@ -1,50 +1,20 @@
 <?php
 
+//cette ligne active la fonction pour récupérer les styles des thémes parent et enfant
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
+//cette fonction enregistre et charge les styles des thémes parents et enfants
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
-/* shortcodes */
 
-add_shortcode('vignette-titre', 'vignette_titre_func');
-
-function vignette_titre_func($atts){
-
-    $atts = shortcode_atts(array(
-        'src' => "",
-        'titre' => 'Titre'
-    ), $atts, 'vignette-titre');
-
-    ob_start();
-
-    if ($atts['src'] != ""){
-        ?>
-
-        <div class="vignette-titre" style="background-image: url(<?= $atts['src'] ?>)">
-            <h2 class="titre"><?= $atts['titre'] ?></h2>
-    </div>
-
-    <?php
-
-    }
-
-    $output = ob_get_contents();
-    ob_end_clean();
-
-    return $output;
-
-}
-
-if (is_user_logged_in()) {
-    add_filter('wp_nav_menu_items', 'ajout_element_menu', 10, 2);
-}
-
-function ajout_element_menu($items, $args) {
-    if ($args->theme_location == 'topbar_menu') {
+function ajouter_element_menu_admin($items, $args) {
+    // Vérifier si l'utilisateur est connecté et si le menu est celui de la topbar
+    if (is_user_logged_in() && $args->theme_location == 'topbar_menu') {
+        // Créer le nouvel élément de menu "Admin"
         $admin = get_admin_url();
         $admin_item = "<li class='menu-item' id='menu-item-21'><a href='{$admin}'>Admin</a></li>";
 
@@ -61,5 +31,51 @@ function ajout_element_menu($items, $args) {
         // Reconstruire les éléments de menu en chaîne de caractères
         $items = implode('</li>', $menu_items) . '</li>';
     }
+
     return $items;
+}
+
+// Ajouter la fonction au hook wp_nav_menu_items si l'utilisateur est connecté
+if (is_user_logged_in()) {
+    add_filter('wp_nav_menu_items', 'ajouter_element_menu_admin', 10, 2);
+}
+
+/***************************/
+/**------shortcodes-------**/
+/***************************/
+
+// Ajouter le shortcode 'vignette-titre' et lui associer la fonction 'vignette_titre_func'
+add_shortcode('vignette-titre', 'vignette_titre_func');
+
+function vignette_titre_func($atts){
+// Définir les attributs par défaut et les fusionner avec les attributs fournis
+
+    $atts = shortcode_atts(array(
+        'src' => "",
+        'titre' => 'Titre'
+    ), $atts, 'vignette-titre');
+
+    //commence la mise en mémoire des informations
+    ob_start();
+
+    // Vérifier si l'attribut 'src' n'est pas vide et applique la condition
+    if ($atts['src'] != ""){
+        ?>
+
+        <div class="vignette-titre" style="background-image: url(<?= $atts['src'] ?>)">
+            <h2 class="titre"><?= $atts['titre'] ?></h2>
+    </div>
+
+    <?php
+
+    }
+
+    //Récupére les infos mise en mémoire tampon
+    $output = ob_get_contents();
+    //arrête la mise en mémoire tampon
+    ob_end_clean();
+
+    //retourne le html complété
+    return $output;
+
 }
